e086310e88ce242bae71a96ee39dd92b8c0ec1f7
Sean Cosentino <Sparkcollaboration@gmail.com>
2015-04-28 17:16:49 -0400

    First Chanegs - bug fixes for heroku

--- server.rb
+++ server.rb
@@ -3,11 +3,14 @@ require 'httparty'
 require 'json'
 require 'nokogiri'
 
+
+set :public_folder, File.dirname(__FILE__) + '/public/Portfolio'
+
 #Why/how do i add visuals to this page?
 
-get '/' do			#get methods takes two argumetns string: website 
-	p "#{Time.now}"	#block of ruby code executed when 
-end
+# get '/' do			#get methods takes two argumetns string: website 
+# 	p "#{Time.now}"	#block of ruby code executed when 
+# end
 
 get '/g' do
   "Hello World"
